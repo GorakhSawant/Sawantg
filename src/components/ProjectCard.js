75e1b13c8e51ea9573d7a09e1dd5925c2f6dbb71
GorakhSawant <gorakh.r.sawant@gmail.com>
2024-09-26 23:23:59 +0530

    project structure

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,17 @@
+// src/components/ProjectCard.js
+import React from 'react';
+
+const ProjectCard = ({ title, description, link, imgSrc }) => {
+    return (
+        <div className="bg-white shadow-md rounded-lg overflow-hidden">
+            <img src={imgSrc} alt={title} className="w-full h-40 object-cover" />
+            <div className="p-4">
+                <h3 className="font-bold text-lg">{title}</h3>
+                <p>{description}</p>
+                <a href={link} className="text-blue-500">View Project</a>
+            </div>
+        </div>
+    );
+};
+
+export default ProjectCard;
